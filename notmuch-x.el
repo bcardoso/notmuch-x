aacfd0644d2955c81f62a5b6f9ad4ce45f506ad5
bruno <cardoso.bc@gmail.com>
2023-12-09 00:16:06 -0300

    refactor notmuch-x-update & notmuch-x-modeline-indicator

--- notmuch-x.el
+++ notmuch-x.el
@@ -1,10 +1,10 @@
 ;;; notmuch-x.el --- Notmuch extensions -*- lexical-binding: t -*-
 
-;; Copyright (C) 2021-2022 Bruno Cardoso
+;; Copyright (C) 2021-2023 Bruno Cardoso
 
 ;; Author: Bruno Cardoso <cardoso.bc@gmail.com>
 ;; URL: https://github.com/bcardoso/notmuch-x
-;; Version: 0.1
+;; Version: 0.2
 ;; Package-Requires: ((emacs "27.2"))
 
 ;; This file is NOT part of GNU Emacs.
@@ -39,47 +39,52 @@
   "Group for `notmuch-x' customizations."
   :group 'notmuch)
 
-(defcustom notmuch-x--notmuch-update-interval 15
+(defcustom notmuch-x-auto-update-interval 15
   "Number of minutes between each mail retrieval by `notmuch-x-update'."
   :tag "Notmuch retrieval interval"
   :group 'notmuch-x
   :type 'integer)
 
-(defcustom notmuch-x--notmuch-update-buffer
-  "*notmuch-update*"
+(defcustom notmuch-x-update-buffer "*notmuch-update*"
   "Name of the buffer for the `notmuch-x-update' output."
   :tag "Notmuch retrieval output buffer"
   :group 'notmuch-x
   :type 'string)
 
-(defcustom notmuch-x--notmuch-update-notify t
+(defcustom notmuch-x-update-notify t
   "Notify in echo area when `notmuch-x-update' starts and finishes."
   :tag "Notmuch retrieval output buffer"
   :group 'notmuch-x
   :type 'string)
 
-(defcustom notmuch-x--search-query-new-mail
+(defcustom notmuch-x-search-query-new-mail
   "date:2d..now and tag:unread and not tag:trash"
-  "Search query for new mail run by `notmuch-x-mode-line-indicator'."
+  "Search query for new mail for `notmuch-x-indicator-mode'."
   :tag "Search query for new mail"
   :group 'notmuch-x
   :type 'string)
 
-(defcustom notmuch-x--indicator-timer-update-interval 30
-  "Number of seconds to update the `notmuch-x-mode-line-indicator'."
+(defcustom notmuch-x-mail-indicator-idle-interval 15
+  "Number of idle seconds to update the `notmuch-x-modeline-indicator'."
   :tag "Update interval for the mode line indicator"
   :group 'notmuch-x
   :type 'integer)
 
-(defcustom notmuch-x--view-part-temp-dir
+(defcustom notmuch-x-mail-indicator-auto-hide t
+  "Hide the `notmuch-x-modeline-indicator' when there are no new messages."
+  :tag "Update interval for the mode line indicator"
+  :group 'notmuch-x
+  :type 'boolean)
+
+(defcustom notmuch-x-view-part-temp-dir
   "/tmp/"
   "Temporary directory to save MIME parts."
   :tag "Temporary directory to save MIME parts"
   :group 'notmuch-x
   :type 'string)
 
-(defcustom notmuch-x--auto-update t
-  "When non-nil, `notmuch-x-run-notmuch' will start `notmuch-x-update-timer'."
+(defcustom notmuch-x-auto-update t
+  "When non-nil, `notmuch-x-run-notmuch' will start the auto update timer."
   :tag "Auto update"
   :group 'notmuch-x
   :type 'string)
@@ -87,72 +92,29 @@
 
 ;;;; Update Database
 
-(defvar notmuch-x--update-timer nil
-  "The update timer set by `notmuch-x-update-timer'.")
-
-;;;###autoload
-(defun notmuch-x-run-notmuch ()
-  "Run `notmuch' and `notmuch-x-toggle-mode-line-indicator'. When user
-option `notmuch-x--auto-update' is non-nil, also run `notmuch-x-update-timer'."
-  (interactive)
-  (if (not notmuch-x--update-timer)
-      (progn (if notmuch-x--auto-update (notmuch-x-update-timer))
-             (notmuch-x-update-dwim)
-             (notmuch-x-toggle-mode-line-indicator t)))
-  (notmuch))
-
-(defvar notmuch-x--notmuch-update-notify-if-interactive nil
+(defvar notmuch-x-update-notify-if-interactive nil
   "Notify update when `notmuch-x-update-dwim' is called interactively.")
 
 (defun notmuch-x--update-notify (str)
-  "Notify update status."
-  (when (or notmuch-x--notmuch-update-notify-if-interactive
-            notmuch-x--notmuch-update-notify)
+  "Notify update status STR."
+  (when (or notmuch-x-update-notify-if-interactive
+            notmuch-x-update-notify)
     (message str)))
-
-;;;###autoload
-(defun notmuch-x-update-dwim ()
-  "Retrieve mail and update notmuch database. With ARG, start timer."
-  (interactive)
-  (if current-prefix-arg
-      (progn
-        (if (not notmuch-x--update-timer)
-            (notmuch-x-update-timer)
-          (message "notmuch-x-update-timer is already running.")))
-    (setq notmuch-x--notmuch-update-notify-if-interactive
-          (called-interactively-p 'interactive))
-    (notmuch-x-update)))
 
 (defun notmuch-x-update ()
   "Retrieve mail and update notmuch database."
   (interactive)
   (notmuch-x--update-notify "[notmuch] Retrieving mail...")
   (make-process :name     "notmuch-update"
-                :buffer   notmuch-x--notmuch-update-buffer
+                :buffer   notmuch-x-update-buffer
                 :command  '("notmuch" "new")
                 :sentinel 'notmuch-x-update-sentinel)
-  (with-current-buffer notmuch-x--notmuch-update-buffer
+  (with-current-buffer notmuch-x-update-buffer
     (special-mode)))
-
-(defvar notmuch-x--update-timer nil
-  "The notmuch update timer.")
-
-(defun notmuch-x-update-timer ()
-  "Periodic mail retrieval according to `notmuch-x--notmuch-update-interval'"
-  (interactive)
-  (setq notmuch-x--update-timer
-        (run-at-time t (* 60 notmuch-x--notmuch-update-interval)
-                     #'notmuch-x-update)))
-
-(defun notmuch-x-update-timer-stop ()
-  "Cancel `notmuch-x-update-timer'"
-  (interactive)
-  (cancel-function-timers #'notmuch-x-update)
-  (setq notmuch-x--update-timer nil))
 
 (defun notmuch-x-update-sentinel (process event)
   "Sentinel to run after notmuch update."
-  (with-current-buffer notmuch-x--notmuch-update-buffer
+  (with-current-buffer notmuch-x-update-buffer
     (setq-local buffer-read-only nil)
     (goto-char (point-max)))
   (if (string= event "finished\n")
@@ -161,14 +123,63 @@ option `notmuch-x--auto-update' is non-nil, also run `notmuch-x-update-timer'."
         (with-temp-buffer
           (insert (format "\nLast database update: %s\n\n"
                           (format-time-string "%F %T")))
-          (append-to-buffer notmuch-x--notmuch-update-buffer
-                            (point-min) (point-max))))
-    (progn
-      (message "[notmuch] Something went wrong.")
-      (switch-to-buffer notmuch-x--notmuch-update-buffer)))
-  (with-current-buffer notmuch-x--notmuch-update-buffer
+          (append-to-buffer notmuch-x-update-buffer
+                            (point-min) (point-max)))
+        (when notmuch-x-indicator-mode (notmuch-x-indicator-update)))
+    (message "[notmuch] Something went wrong.")
+    (switch-to-buffer notmuch-x-update-buffer))
+  (with-current-buffer notmuch-x-update-buffer
     (setq-local buffer-read-only t))
-  (setq notmuch-x--notmuch-update-notify-if-interactive nil))
+  (setq notmuch-x-update-notify-if-interactive nil))
+
+(defvar notmuch-x--auto-update-timer nil
+  "The auto update timer for `notmuch-x-update'.")
+
+(defun notmuch-x-start-auto-update-timer ()
+  "Start auto update timer for `notmuch-x-update'."
+  (interactive)
+  (if (not notmuch-x--auto-update-timer)
+      (setq notmuch-x--auto-update-timer
+            (run-with-timer 1 (* 60 notmuch-x-auto-update-interval)
+                            #'notmuch-x-update))
+    (message "[notmuch] Auto update timer is already running.")))
+
+(defun notmuch-x-stop-auto-update-timer ()
+  "Stop auto update timer for `notmuch-x-update'."
+  (interactive)
+  (if (not notmuch-x--auto-update-timer)
+      (message "[notmuch] There is no auto update timer running.")
+    (cancel-function-timers #'notmuch-x-update)
+    (setq notmuch-x--auto-update-timer nil)
+    (message "[notmuch] Auto update timer canceled.")))
+
+;;;###autoload
+(define-minor-mode notmuch-x-auto-update-mode
+  "Toggle the auto update timer."
+  :init-value nil :global t :group 'notmuch-x
+  (if notmuch-x-auto-update-mode
+      (notmuch-x-start-auto-update-timer)
+    (notmuch-x-stop-auto-update-timer)))
+
+;;;###autoload
+(defun notmuch-x-update-dwim (&optional arg)
+  "Retrieve mail and update notmuch database. With ARG, start timer."
+  (interactive "P")
+  (when arg (notmuch-x-start-auto-update-timer))
+  (setq notmuch-x-update-notify-if-interactive
+        (called-interactively-p 'interactive))
+  (notmuch-x-update))
+
+;;;###autoload
+(defun notmuch-x-run-notmuch ()
+  "Run `notmuch' and activate `notmuch-x-indicator-mode'.
+When `notmuch-x-auto-update' is non-nil, also start auto update timer."
+  (interactive)
+  (setq notmuch-x-update-notify-if-interactive
+        (called-interactively-p 'interactive))
+  (notmuch-x-auto-update-mode notmuch-x-auto-update)
+  (notmuch-x-indicator-mode +1)
+  (notmuch))
 
 
 ;;;; Mail Indicator
@@ -177,52 +188,57 @@ option `notmuch-x--auto-update' is non-nil, also run `notmuch-x-update-timer'."
   "Search query for new mail."
   (interactive)
   (delete-other-windows)
-  (notmuch-search notmuch-x--search-query-new-mail nil))
+  (notmuch-search notmuch-x-search-query-new-mail))
 
-(defun notmuch-x--mode-string (count)
-  "Add properties to mode line indicator."
-  (let ((mail-icon (find-image
-                    '((:type xpm :file "letter.xpm" :ascent center)
-                      (:type pbm :file "letter.pbm" :ascent center)))))
-    (when (not (string= count "0"))
-      (propertize "@M"
-                  'display (when (display-graphic-p) mail-icon)
-                  'mouse-face 'mode-line-highlight
-                  'keymap '(mode-line keymap
-                                      (mouse-1 . notmuch-x-search-new-mail))
-                  'help-echo "New mail"))))
+(defun notmuch-x-new-mail-counter (&optional query)
+  "Return the mail count for QUERY or `notmuch-x-search-query-new-mail'."
+  (string-to-number (notmuch-saved-search-count
+                     (or query notmuch-x-search-query-new-mail))))
 
-(defvar notmuch-x--indicator-timer nil
-  "The mode line indicator timer.")
+(defun notmuch-x-new-mail-p (&optional query)
+  "Return non-nil when there is new mail for optional QUERY.
+Default query is defined by `notmuch-x-search-query-new-mail'."
+  (> (notmuch-x-new-mail-counter query) 0))
 
-(defvar notmuch-x-mode-line-indicator nil
-  "Mode line indicator element.")
+;; (notmuch-saved-search-count "date:1d..now and tag:unread and tag:inbox")
 
-(defun notmuch-x-mode-line-indicator (&optional disable)
-  "Toggle the mode line indicator element."
-  (let* ((count (notmuch-saved-search-count notmuch-x--search-query-new-mail))
-         (indicator (notmuch-x--mode-string count)))
-    (cond ((and (not disable) (>= (string-to-number count) 1))
-           (if (not notmuch-x--indicator-timer)
-               (setq notmuch-x--indicator-timer
-                     (run-at-time t notmuch-x--indicator-timer-update-interval
-                                  #'notmuch-x-mode-line-indicator)))
-           (add-to-list 'global-mode-string indicator)
-           (setq notmuch-x-mode-line-indicator indicator))
-          (t
-           (cancel-function-timers #'notmuch-x-mode-line-indicator)
-           (setq global-mode-string
-                 (delete notmuch-x-mode-line-indicator global-mode-string))
-           (setq notmuch-x--indicator-timer nil)
-           (setq notmuch-x-mode-line-indicator nil)))))
+(defvar notmuch-x--modeline-indicator-string nil)
+
+(defvar-local notmuch-x-modeline-indicator
+    '(:eval notmuch-x--modeline-indicator-string))
+
+(put 'notmuch-x-modeline-indicator 'risky-local-variable t)
+
+(defun notmuch-x-indicator-update ()
+  "Update notmuch mode line indicator."
+  (setq notmuch-x--modeline-indicator-string
+        (when (or (notmuch-x-new-mail-p)
+                  (not notmuch-x-mail-indicator-auto-hide))
+          (propertize
+           "@M"
+           'display (when (display-graphic-p)
+                      (find-image
+                       '((:type xpm :file "letter.xpm" :ascent center)
+                         (:type pbm :file "letter.pbm" :ascent center))))
+           'mouse-face 'mode-line-highlight
+           'keymap '(mode-line keymap (mouse-1 . notmuch-x-search-new-mail))
+           'help-echo (if (notmuch-x-new-mail-p)
+                          (let ((num (notmuch-x-new-mail-counter)))
+                            (format "%s message%s" num (if (> num 1) "s" "")))
+                        "No new mail")))))
 
 ;;;###autoload
-(defun notmuch-x-toggle-mode-line-indicator (&optional force-on)
-  "Toggle mode line indicator for new mail."
-  (interactive)
-  (if (and (not force-on) notmuch-x-mode-line-indicator)
-      (notmuch-x-mode-line-indicator 'disable)
-    (notmuch-x-mode-line-indicator)))
+(define-minor-mode notmuch-x-indicator-mode
+  "Toggle the `notmuch-x-modeline-indicator'."
+  :init-value nil :global t :group 'notmuch-x
+  (if notmuch-x-indicator-mode
+      (progn
+        (run-with-idle-timer notmuch-x-mail-indicator-idle-interval
+                             t #'notmuch-x-indicator-update)
+        (add-to-list 'global-mode-string notmuch-x-modeline-indicator))
+    (cancel-function-timers #'notmuch-x-indicator-update)
+    (setq global-mode-string
+          (delete notmuch-x-modeline-indicator global-mode-string))))
 
 
 ;;;; Tagging
@@ -238,7 +254,7 @@ option `notmuch-x--auto-update' is non-nil, also run `notmuch-x-update-timer'."
           (notmuch-search-refresh-view)))))
 
 (defun notmuch-x-tag-thread-dwim (tags &optional show-next)
-  "Set TAGS to thread."
+  "Set TAGS to thread. When SHOW-NEXT is non-nil, go to next thread."
   (interactive)
   (let ((notmuch-archive-tags tags))
     (if (eq major-mode 'notmuch-search-mode)
@@ -263,7 +279,7 @@ option `notmuch-x--auto-update' is non-nil, also run `notmuch-x-update-timer'."
 ;;;; Search mode
 
 (defun notmuch-x-edit-current-search ()
-  "Edit current notmuch search query"
+  "Edit current notmuch search query."
   (interactive)
   (let ((query (read-from-minibuffer "Notmuch search: "
                                      notmuch-search-query-string)))
@@ -303,10 +319,10 @@ option `notmuch-x--auto-update' is non-nil, also run `notmuch-x-update-timer'."
 (defvar notmuch-x--button-url-regexp (concat
                                       "\\[ .*\\]$\\|"
                                       browse-url-button-regexp)
-  "Regexp for searching buttons and link on notmuch-show")
+  "Regexp for searching buttons and link on notmuch-show.")
 
 (defun notmuch-x-next-button-or-link (&optional backwards)
-  "Goto to the next button or link."
+  "Goto to the next button or link. When BACKWARDS is non-nil, go backwards."
   (interactive)
   (let ((button-pos) (link-pos) (match))
     (setq match (if backwards
@@ -334,7 +350,7 @@ option `notmuch-x--auto-update' is non-nil, also run `notmuch-x-update-timer'."
   "Save MIME part at point in temp dir and open with browser."
   (interactive)
   (let ((handle (notmuch-show-current-part-handle))
-        (file (concat notmuch-x--view-part-temp-dir
+        (file (concat notmuch-x-view-part-temp-dir
                       (format "notmuch-mime-part.")
                       (format "%s." (notmuch-show-get-timestamp))
                       (replace-regexp-in-string "\\([^[:print:]]\\|/\\)" ""
